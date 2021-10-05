006319061a906b6fe037c8e32b148cba696287ff
Yukyan <yukyanjpn@gmail.com>
2021-10-05 22:25:27 +0900

    parse関数をわかりやすくした

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from icecream import ic
 
 
 def parse(text):
-    mecab = MeCab.Tagger()
-    parseds = [[parsed.split("\t")[0], parsed.split("\t")[1].split(",")[0]]
-               for parsed in mecab.parse(text).split("\n")[:-2]]
-    return parseds
+    mecab = MeCab.Tagger("")
+    node = mecab.parseToNode(text).next
+    parse_texts = []
+
+    while node.next:
+        word = node.surface
+        part_of_speech = node.feature.split(",")[0]
+        parse_texts.append([word, part_of_speech])
+        node = node.next
+
+    return parse_texts
 
 
 ic(parse("卒業研究を頑張ります"))
