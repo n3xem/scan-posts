2a5e11771b691e5552296f3180dd8da5cca441f3
Yukyan <yukyanjpn@gmail.com>
2021-10-05 22:10:48 +0900

    文章を形態素解析して品詞を求める

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import MeCab
-mecab = MeCab.Tagger()
-print(mecab.parse("卒業研究を頑張ります"))
+from icecream import ic
+
+
+def parse(text):
+    mecab = MeCab.Tagger()
+    parseds = [[parsed.split("\t")[0], parsed.split("\t")[1].split(",")[0]]
+               for parsed in mecab.parse(text).split("\n")[:-2]]
+    return parseds
+
+
+ic(parse("卒業研究を頑張ります"))
